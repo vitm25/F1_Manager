4e2b934b54a3007c58bbed759c58e5d11d334ed1
Marek Vít <vitm25@spstrutnov.cz>
2026-01-27 12:02:58 +0100

    začínáme velice pomalinku a polehoučku

--- manager.py
+++ manager.py
@@ -5,13 +5,22 @@ pygame.init() # spusteni knihovny
 
 rozliseni_okna = (1000, 800)
 barvy_pozadi = (0, 0, 0,)
+FPS = 60
 
 #vykreslení okna
 okno = pygame.display.set_mode(rozliseni_okna)
-pygame.display.set_caption("F1 manažér")
+pygame.display.set_caption("F1 manažer")
+
+clock = pygame.time.Clock()
+
+GAME_STATE_RACE = "RACE"
+game_state = GAME_STATE_RACE
+
+race_time = 0.0
 
 # vykreslovaci smycka
 while True:
+    delta_time = clock.tick(FPS) / 1000
     # kontrola probehlych udalosti
     for udalost in pygame.event.get():
         # kontrola udalosti "vypnout"
@@ -20,4 +29,3 @@ while True:
             pygame.quit()
             # vypnuti programu
             sys.exit()
-
