b098fa33be84e17e5a8f07299be2682e210ff967
Marek Vít <vitm25@spstrutnov.cz>
2026-03-08 10:39:45 +0100

    máme tratě v 1080x1080

--- convert_tracks.py
+++ convert_tracks.py
@@ -1,4 +1,6 @@
 import os
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
 
 input_folder = "tracks_svg"
 output_folder = "tracks_png"
@@ -6,13 +8,15 @@ output_folder = "tracks_png"
 os.makedirs(output_folder, exist_ok=True)
 
 for file in os.listdir(input_folder):
+
     if file.endswith(".svg"):
-        
+
         svg_path = os.path.join(input_folder, file)
         png_path = os.path.join(output_folder, file.replace(".svg", ".png"))
-        
-        os.system(f'inkscape "{svg}" --export-type=png --export-width=1920 --export-height=1080 --export-filename="{png}"')
-        
-        print(f"Převedeno: {file}")
-        
+
+        drawing = svg2rlg(svg_path)
+        renderPM.drawToFile(drawing, png_path, fmt="PNG")
+
+        print("Converted:", file)
+
 print("Hotovo!")
